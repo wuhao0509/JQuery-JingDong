8f16cce3f82f5544f1631856400ccb015f50b43d
wuhao0509 <303459939@qq.com>
2021-01-18 20:08:30 +0800

    添加轮播图

--- fs/index.js
+++ fs/index.js
@@ -1,2 +1,16 @@
 $('.fs-1').load('/fs/fs1.html')
 $('.fs-3').load('/fs/fs3.html')
+
+// 添加轮播图
+$('.slider-banner-wrapper').swiper({
+    list: $('.banner-img'),
+    width: 590,
+    height: 470
+})
+$('.slider-recommend-wrapper').swiper({
+    list: $(".focus-item__recommend"),
+    width: 190,
+    height: 470,
+    showChangeBtn: false,
+    showSpotBtn: false
+})
